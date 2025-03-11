c15080537490efede404c288232d150c985432d8
Илья <>
2025-03-11 19:12:31 +0400

    fix

--- server.js
+++ server.js
@@ -12,6 +12,28 @@ app.use(express.json()) // Для обработки JSON-данных
 const TELEGRAM_BOT_TOKEN = '7673196064:AAGoAJFZpKnb63QDhkC_pcxrnT7Q3RYG858'
 const TELEGRAM_CHAT_ID = 1114712689
 
+export const getChatId = async () => {
+	const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/getUpdates`
+
+	try {
+		const response = await axios.post(url)
+		console.log('Ответ от Telegram:', response.data)
+
+		// Извлекаем chat_id из ответа
+		if (response.data.ok && response.data.result.length > 0) {
+			const chatId = response.data.result[length - 1].message.chat.id
+			console.log('Ваш chat_id:', chatId)
+			TELEGRAM_CHAT_ID = chatId
+		} else {
+			console.log('Нет новых сообщений или обновлений.')
+			return null
+		}
+	} catch (error) {
+		console.error('Ошибка:', error)
+		throw error
+	}
+}
+getChatId()
 // Функция для отправки сообщения в Telegram
 const sendTelegramMessage = async text => {
 	try {
